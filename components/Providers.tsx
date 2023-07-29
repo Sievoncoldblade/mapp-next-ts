acbab59e3a3b7c82b17e32c1f95a480d3c839d69
Euphel <jlcsievon@gmail.com>
2023-07-29 18:54:10 +0800

    added prisma

---
+++ components/Providers.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+
+interface Props {
+  children: ReactNode;
+}
+
+function Providers({ children }: Props) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
+
+export default Providers;
